11b5f45943b4fc7e6fb1b453c4510098714c6795
Avinashkubsad <avi.kubsad14@gmail.com>
2025-11-12 22:05:51 +0530

    display in console when clicked on submit

--- todolist_app/src/components/TodoForm.jsx
+++ todolist_app/src/components/TodoForm.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
+
 
 export const TodoForm = () => {
+  const [value,setvalue] = useState("")     //declaree the setvalue, and value to store the input data
+
+
+  const handlesubmit = e =>{
+    e.preventDefault();    // to prevent page from reloading on submit
+
+    console.log(value)
+  }
   return (
-    <form className='Todoform'>
-        <input type ="text" className='todo-input' placeholder="Add a task to do today"/>
-        <button className="todo-button">
+    <form className='Todoform' onSubmit={handlesubmit}>
+        <input type ="text" className='todo-input' placeholder="Add a task to do today"
+        onChange= {(e) => setvalue(e.target.value)}/>
+        <button  type ="submit" className="todo-btn">
             ADD TASK
         </button>
     </form>
