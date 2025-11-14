bf785031ad2d9ff234b7b281d1d1f9acbe820d2a
Avinashkubsad <avi.kubsad14@gmail.com>
2025-11-14 21:30:00 +0530

    hanges in todowrapper

--- todolist_app/src/components/TodoWrapper.jsx
+++ todolist_app/src/components/TodoWrapper.jsx
@@ -6,6 +6,10 @@ export const TodoWrapper = () => {
   return (
     <>
     <h1>Hi Avinash Welcome Buddy!!</h1>
+
+    <h2>
+      Ready to To do something useful Todayy!!
+    </h2>
     <div className='TodoWrapper'>
 
       <TodoForm>
