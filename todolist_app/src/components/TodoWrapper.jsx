30c455903b79f1a8bc27b54c31dce7828d00222a
Avinashkubsad <avi.kubsad14@gmail.com>
2025-11-24 20:34:35 +0530

    uuid is used to create a new id

--- todolist_app/src/components/TodoWrapper.jsx
+++ todolist_app/src/components/TodoWrapper.jsx
@@ -1,8 +1,16 @@
 import React,{useState} from 'react'
 import { TodoForm } from './TodoForm'
+import {v4 as uuidv4} from 'uuid'   // to generate unique id for each todo item
+uuidv4()   // initialize the uuid function
 
 export const TodoWrapper = () => {
   const[todos,setTodos] = useState([])
+
+
+  const addTodo = todo =>{
+      setTodos([...todos, {id:uuidv4(),task :todo,completed:false}])
+
+  }
   return (
     <>
     <h1>Hi Avinash Welcome Buddy!!</h1>
@@ -11,8 +19,7 @@ export const TodoWrapper = () => {
     </h2>
     <div className='TodoWrapper'>
 
-   <TodoForm >
-  {/* addTodo is a property & {addTodo} is a function */}
+   <TodoForm  addTodo={addTodo}>{/* addTodo is a property & {addTodo} is a function */}
 </TodoForm>
 
     </div>
